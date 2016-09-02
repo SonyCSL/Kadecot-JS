eb0ca59a64ad5975c6b630f659bab24f2ed93c56
3846masa <3846masahiro+git@gmail.com>
2016-09-02 18:05:32 +0900

    Add body-parser and cors

--- rest-server/index.js
+++ rest-server/index.js
@@ -1,14 +1,42 @@
 'use strict';
 
 const express = require('express');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+const multiparty = require('multiparty');
 const path = require('path');
 
 class RestServer {
   constructor () {
     this._app = express();
 
+    this._app.use(cors({
+      origin: '*',
+      methods: [ 'GET', 'POST', 'PUT', 'DELETE' ]
+    }));
+    this.setBodyParser();
     // Static files
     this._app.use(express.static(path.resolve(__dirname, 'statics')));
+  }
+
+  setBodyParser () {
+    this._app.use(bodyParser.urlencoded());
+    this._app.use(bodyParser.json());
+    this._app.use(bodyParser.raw({
+      type: 'multipart/*'
+    }), (req, res, next) => {
+      const form = new multiparty.Form();
+      form.parse(req, (err, fields) => {
+        fields = fields || {};
+        Object.keys(fields).forEach((name) => {
+          const field = fields[name] || [];
+          if (field.length > 0) {
+            req.query[name] = field[0];
+          }
+        });
+        next();
+      });
+    });
   }
 
   listen (port, cb) {
