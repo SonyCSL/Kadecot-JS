25badfee7c2b516adb188f005edf01254cb008c7
kadecot at CSLLiva <kadecot@CSLLiva>
2016-11-24 18:05:56 +0900

    Deleted unncessary files

--- v1/htdocs/settings.js
+++
@@ -1,6 +0,0 @@
-// Kadecot cloud terminal registartion URL (Eg. https://[cloud host name]/terminal_regist.html
-var REGIST_URL = 'https://dev.hems.gallery/terminal_regist.html';
-// Random UUID string (typically 45 random characters)
-var UUID = 'BQNFovNovpEyNxDW0NgNgZoFbDr4FNEPNVo77iyezEOJt';
-// Arbitrary name of your Kadecot user (Eg. Arbitrary string is accepted, but you may want to change to your favorite name
-var ACCOUNT_NAME = 'LivaKadecot';
